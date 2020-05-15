02c1272b452a01888362e1d9f8c727f973e99fc7
Regis Relland <regis.relland@insee.fr>
2020-05-15 09:24:38 +0200

    Initial commit

---
+++ R/mafonction.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param x
+#' @param na.value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fix_missing <- function(x, na.value) {
+  x[x == na.value] <- NA
+  x
+}
+usethis::edit_r_environ("user")
